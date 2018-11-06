5d4c468692280b8c886d9957c62ae669855decf2
Jan Mercl <0xjnml@gmail.com>
2018-11-06 17:27:39 +0100

    Make modernc.org the repository root

--- httpfs.go
+++ httpfs.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Package httpfs implements http.FileSystem on top of a map[string]string.
-package httpfs
+package httpfs // import "modernc.org/httpfs"
 
 import (
 	"fmt"
