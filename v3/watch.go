6dc25496860d642ce89a6514e5da00895540938c
Jan Mercl <0xjnml@gmail.com>
2020-07-13 19:05:07 +0200

    WIPS

--- v3/watch.go
+++ v3/watch.go
@@ -4,12 +4,67 @@
 
 package crt // import "modernc.org/crt/v3"
 
-func Watch(args ...interface{}) {
+import (
+	"fmt"
+	"sort"
+)
+
+const (
+	watchDepth = 2
+)
+
+type watchUintptr struct {
+	p *uintptr
+	s string
+	v uintptr
+}
+
+var watches = map[interface{}]interface{}{}
+
+func Watch(x ...interface{}) {
 	if !dmesgs {
 		return
 	}
 
-	if len(args) != 0 {
-		dmesg("%s: %v", origin(2), args)
+	if len(x) != 0 {
+		dmesg("%s: %v", origin(watchDepth), x)
 	}
+	var a []string
+	for _, w := range watches {
+		dmesg("%s", origin(watchDepth))
+		switch x := w.(type) {
+		case *watchUintptr:
+			v := *x.p
+			if v != x.v {
+				a = append(a, fmt.Sprintf("%v: %s is now %#x, was %#x", origin(watchDepth), x.s, v, x.v))
+				x.v = v
+			}
+		default:
+			panic(todo("%T", x))
+		}
+	}
+	if len(a) == 0 {
+		return
+	}
+
+	sort.Strings(a)
+	for _, v := range a {
+		dmesg("%s", v)
+	}
+}
+
+func WatchUintptr(p *uintptr, s string) {
+	if !dmesgs {
+		return
+	}
+
+	if s == "" {
+		delete(watches, p)
+		return
+	}
+
+	v := *p
+	w := &watchUintptr{p, s, v}
+	watches[p] = w
+	dmesg("%v: %s is initially %#x\n", origin(watchDepth), w.s, w.v)
 }
