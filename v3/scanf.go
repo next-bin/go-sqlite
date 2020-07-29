e030fb923477f1b8179b5c7bd891cf98a7844d3f
Jan Mercl <0xjnml@gmail.com>
2020-07-29 20:18:16 +0200

    refactor printf

---
+++ v3/scanf.go
@@ -0,0 +1,5 @@
+// Copyright 2020 The CRT Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package crt // import "modernc.org/crt/v3"
