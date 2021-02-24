4031d0711ce6c5fed35606d3a7bf66181ba4a736
Jan Mercl <0xjnml@gmail.com>
2021-02-24 16:52:46 +0100

    more fixes

--- httpfs.go
+++ httpfs.go
@@ -41,8 +41,10 @@ func (f *FileSystem) Open(name string) (fi http.File, err error) {
 	if strings.HasSuffix(name, "/") {
 		dir := make([]string, 0) // Must be non-nil.
 		m := map[string]struct{}{}
+		ok := false
 		for k := range f.files {
 			if strings.HasPrefix(k, name) {
+				ok = true
 				k = k[len(name):]
 				parts := strings.Split(k, "/")
 				name := parts[0]
@@ -63,8 +65,12 @@ func (f *FileSystem) Open(name string) (fi http.File, err error) {
 				}
 			}
 		}
+		if !ok {
+			return nil, fmt.Errorf("no such directory: %q", name)
+		}
+
 		sort.Strings(dir)
-		// fmt.Printf("%q -> %v %v\n", name, len(dir), dir) //TODO-
+		//fmt.Printf("%q -> %v %v\n", name, len(dir), dir) //TODO-
 		return &file{FileSystem: f, name: name, dir: dir, mode: os.ModeDir}, nil
 	}
 
@@ -86,7 +92,7 @@ type file struct {
 }
 
 func (f *file) Close() (err error)               { return nil }
-func (f *file) IsDir() (r bool)                  { return f.dir != nil }
+func (f *file) IsDir() (r bool)                  { return strings.HasSuffix(f.name, "/") }
 func (f *file) Mode() (r os.FileMode)            { return f.mode }
 func (f *file) ModTime() (r time.Time)           { return f.modTime }
 func (f *file) Name() (r string)                 { return f.name }
@@ -95,7 +101,7 @@ func (f *file) Stat() (r os.FileInfo, err error) { return f, nil }
 func (f *file) Sys() (r interface{})             { return nil }
 
 func (f *file) Read(b []byte) (n int, err error) {
-	if int(f.off) >= len(f.s) {
+	if f.off >= int64(len(f.s)) {
 		return 0, io.EOF
 	}
 
@@ -112,6 +118,9 @@ func (f *file) Readdir(count int) (r []os.FileInfo, err error) {
 		}
 
 		name := path.Join(f.name, fn)
+		if strings.HasSuffix(fn, "/") {
+			name += "/"
+		}
 		s := f.files[name]
 		r = append(r, &file{name: name, s: s})
 	}
@@ -119,6 +128,7 @@ func (f *file) Readdir(count int) (r []os.FileInfo, err error) {
 }
 
 func (f *file) Seek(offset int64, whence int) (r int64, err error) {
+	off := f.off
 	switch whence {
 	case os.SEEK_SET:
 		f.off = offset
@@ -127,8 +137,9 @@ func (f *file) Seek(offset int64, whence int) (r int64, err error) {
 	case os.SEEK_END:
 		f.off = int64(len(f.s)) + offset
 	}
-	if f.off < 0 || f.off >= int64(len(f.s)) {
-		err = fmt.Errorf("invalid offset")
+	if f.off < 0 || f.off > int64(len(f.s)) {
+		err = fmt.Errorf("invalid offset (%#x/%#x, whence %v)", f.off, len(f.s), whence)
+		f.off = off
 	}
 	return f.off, err
 }
