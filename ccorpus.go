7c582e89321426c2c4278cff10aa7c30d3c941db
Jan Mercl <0xjnml@gmail.com>
2022-03-18 14:40:40 +0100

    OK

--- ccorpus.go
+++ ccorpus.go
@@ -3,16 +3,11 @@
 // license that can be found in the LICENSE file.
 
 // Package ccorpus2 provides a test corpus of C code.
-package ccorpus // import "modernc.org/ccorpus2"
+package ccorpus2 // import "modernc.org/ccorpus2"
 
-// import (
-// 	"time"
-//
-// 	"modernc.org/httpfs"
-// )
-//
-// var fs = httpfs.NewFileSystem(assets, time.Now())
-//
-// // FileSystem returns a httpfs.FileSystem containing the test corpus. Use the
-// // Open method of the result to acquire a http.File from a rooted file name.
-// func FileSystem() *httpfs.FileSystem { return fs }
+import (
+	"embed"
+)
+
+//go:embed assets
+var FS embed.FS
