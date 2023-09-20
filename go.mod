200548632c22f8705d799dc3a8bdcf73d05c1f30
Jan Mercl <0xjnml@gmail.com>
2023-09-20 22:49:39 +0200

    update dependencies

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module modernc.org/libtcl8.6
 go 1.20
 
 require (
-	modernc.org/libc/v2 v2.1.3
-	modernc.org/libz v0.0.5
+	modernc.org/libc/v2 v2.1.4
+	modernc.org/libz v0.0.6
 )
 
 require (
@@ -18,14 +18,15 @@ require (
 	golang.org/x/tools v0.10.0 // indirect
 	lukechampine.com/uint128 v1.2.0 // indirect
 	modernc.org/cc/v3 v3.41.0 // indirect
-	modernc.org/cc/v4 v4.14.2 // indirect
+	modernc.org/cc/v4 v4.14.4 // indirect
 	modernc.org/ccgo/v3 v3.16.15 // indirect
-	modernc.org/ccgo/v4 v4.0.5 // indirect
+	modernc.org/ccgo/v4 v4.0.6 // indirect
 	modernc.org/gc/v2 v2.3.0 // indirect
 	modernc.org/libc v1.24.1 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
 	modernc.org/memory v1.7.1 // indirect
 	modernc.org/opt v0.1.3 // indirect
+	modernc.org/sortutil v1.2.0 // indirect
 	modernc.org/strutil v1.2.0 // indirect
 	modernc.org/token v1.1.0 // indirect
 )
--- internal/tcltest/ccgo_linux_amd64.go
+++ internal/tcltest/ccgo_linux_amd64.go
@@ -193678,19 +193678,6 @@ func _FreeChannelInternalRep(tls *libc.TLS, objPtr uintptr) {
 	x_TclpFree(tls, resPtr)
 }
 
-type TCopyState1 = struct {
-	FreadPtr    uintptr
-	FwritePtr   uintptr
-	FreadFlags  int32
-	FwriteFlags int32
-	FtoRead     int64
-	Ftotal      int64
-	Finterp     uintptr
-	FcmdPtr     uintptr
-	FbufSize    int32
-	Fbuffer     [1]int8
-}
-
 type TChannelState1 = struct {
 	FchannelName         uintptr
 	Fflags               int32
@@ -193727,6 +193714,19 @@ type TChannelState1 = struct {
 	FchanMsg             uintptr
 	FunreportedMsg       uintptr
 	Fepoch               int32
+}
+
+type TCopyState1 = struct {
+	FreadPtr    uintptr
+	FwritePtr   uintptr
+	FreadFlags  int32
+	FwriteFlags int32
+	FtoRead     int64
+	Ftotal      int64
+	Finterp     uintptr
+	FcmdPtr     uintptr
+	FbufSize    int32
+	Fbuffer     [1]int8
 }
 
 type TChannel1 = struct {
