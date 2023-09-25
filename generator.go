deaf19a8479a48262f25e5a22d5e18e3fbcd7519
Jan Mercl <0xjnml@gmail.com>
2023-09-25 23:07:42 +0200

    make some progress.2

--- generator.go
+++ generator.go
@@ -34,6 +34,10 @@ func fail(rc int, msg string, args ...any) {
 }
 
 func main() {
+	if goos != "linux" {
+		return
+	}
+
 	if ccgo.IsExecEnv() {
 		if err := ccgo.NewTask(goos, goarch, os.Args, os.Stdout, os.Stderr, nil).Main(); err != nil {
 			fmt.Fprintln(os.Stderr, err)
