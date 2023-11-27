5fb8f2cd5a5da8b1e7276f41815e618e1738c065
Jan Mercl <0xjnml@gmail.com>
2023-11-27 13:23:31 +0100

    freebsd: use gsed

--- generator.go
+++ generator.go
@@ -28,6 +28,8 @@ const (
 var (
 	goos   = runtime.GOOS
 	goarch = runtime.GOARCH
+	target = fmt.Sprintf("%s/%s", goos, goarch)
+	sed    = "sed"
 	j      = fmt.Sprint(runtime.GOMAXPROCS(-1))
 )
 
@@ -42,6 +44,11 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 		}
 		return
+	}
+
+	switch goos {
+	case "freebsd":
+		sed = "gsed"
 	}
 
 	f, err := os.Open(archivePath)
@@ -126,8 +133,8 @@ func main() {
 
 	fn := fmt.Sprintf("ccgo_%s_%s.go", goos, goarch)
 	mustCopyFile(fn, filepath.Join(libRoot, result), nil)
-	util.MustShell(true, "sed", "-i.bak", `s/\<T__\([a-zA-Z0-9][a-zA-Z0-9_]\+\)/t__\1/g`, fn)
-	util.MustShell(true, "sed", "-i.bak", `s/\<x_\([a-zA-Z0-9][a-zA-Z0-9_]\+\)/X\1/g`, fn)
+	util.MustShell(true, sed, "-i.bak", `s/\<T__\([a-zA-Z0-9][a-zA-Z0-9_]\+\)/t__\1/g`, fn)
+	util.MustShell(true, sed, "-i.bak", `s/\<x_\([a-zA-Z0-9][a-zA-Z0-9_]\+\)/X\1/g`, fn)
 	util.MustShell(true, "sh", "-c", "rm *.bak")
 	util.MustShell(true, "cp", filepath.Join(libRoot, "example64.go"), filepath.Join("internal", "example", fn))
 	util.MustShell(true, "cp", filepath.Join(libRoot, "minigzip64.go"), filepath.Join("internal", "minigzip", fn))
