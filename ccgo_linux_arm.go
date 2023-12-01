63eebf04d9e009456ad1965ee39f4a16ce143889
Jan Mercl <0xjnml@gmail.com>
2023-12-01 18:39:47 +0100

    pi32: auto generate

--- ccgo_linux_arm.go
+++ ccgo_linux_arm.go
@@ -1,4 +1,4 @@
-// Code generated for linux/arm by 'generator --libc modernc.org/libc --package-name libsqlite3 --prefix-enumerator=_ --prefix-external=x_ --prefix-field=F --prefix-macro=m_ --prefix-static-internal=_ --prefix-static-none=_ --prefix-tagged-enum=_ --prefix-tagged-struct=T --prefix-tagged-union=T --prefix-typename=T --prefix-undefined=_ -DLONGDOUBLE_TYPE=double -DSQLITE_THREADSAFE=0 -Dpread64=pread -Dpwrite64=pwrite -DNDEBUG -extended-errors -o sqlite3.go sqlite3.c -I/tmp/libsqlite3/sqlite-amalgamation-3370200/ccgo', DO NOT EDIT.
+// Code generated for linux/arm by 'generator --package-name libsqlite3 --prefix-enumerator=_ --prefix-external=x_ --prefix-field=F --prefix-macro=m_ --prefix-static-internal=_ --prefix-static-none=_ --prefix-tagged-enum=_ --prefix-tagged-struct=T --prefix-tagged-union=T --prefix-typename=T --prefix-undefined=_ -ignore-unsupported-alignment -DLONGDOUBLE_TYPE=double -DSQLITE_THREADSAFE=0 -Dpread64=pread -Dpwrite64=pwrite -DNDEBUG -extended-errors -o sqlite3.go sqlite3.c -I/tmp/libsqlite3/sqlite-amalgamation-3370200/ccgo', DO NOT EDIT.
 
 //go:build linux && arm
 // +build linux,arm
@@ -151795,46 +151795,13 @@ func Xsqlite3_sourceid(tls *libc.TLS) (r uintptr) {
 	return __ccgo_ts + 23358
 }
 
-type T_ht = struct {
-	Fcount uint32
-	Fchain uintptr
-}
-
-type Tsqlite3_index_constraint = struct {
-	FiColumn     int32
-	Fop          uint8
-	Fusable      uint8
-	FiTermOffset int32
-}
-
-type TWalSegment = struct {
-	FiNext  int32
-	FaIndex uintptr
-	FaPgno  uintptr
-	FnEntry int32
-	FiZero  int32
-}
-
-type Tsqlite3_index_constraint_usage = struct {
-	FargvIndex   int32
-	Fomit        uint8
-	F__ccgo_pad2 [3]byte
-}
-
-type TsColMap = struct {
-	FiFrom int32
-	FzCol  uintptr
-}
-
-type TIdList_item = struct {
-	FzName uintptr
-	Fidx   int32
-}
-
-type Tsqlite3_index_orderby = struct {
-	FiColumn     int32
-	Fdesc        uint8
-	F__ccgo_pad2 [3]byte
+type TAggInfo_col = struct {
+	FpTab          uintptr
+	FpCExpr        uintptr
+	FiTable        int32
+	FiMem          int32
+	FiColumn       Ti16
+	FiSorterColumn Ti16
 }
 
 type TInLoop = struct {
@@ -151846,13 +151813,23 @@ type TInLoop = struct {
 	F__ccgo_pad5 [3]byte
 }
 
-type TAggInfo_col = struct {
-	FpTab          uintptr
-	FpCExpr        uintptr
-	FiTable        int32
-	FiMem          int32
-	FiColumn       Ti16
-	FiSorterColumn Ti16
+type TIdList_item = struct {
+	FzName uintptr
+	Fidx   int32
+}
+
+type Tsqlite3InitInfo = struct {
+	FnewTnum       TPgno
+	FiDb           Tu8
+	Fbusy          Tu8
+	F__ccgo_align3 [2]byte
+	F__ccgo8       uint8
+	FazInit        uintptr
+}
+
+type TsColMap = struct {
+	FiFrom int32
+	FzCol  uintptr
 }
 
 type TAggInfo_func = struct {
@@ -151863,13 +151840,45 @@ type TAggInfo_func = struct {
 	FiDistAddr int32
 }
 
-type Tsqlite3InitInfo = struct {
-	FnewTnum       TPgno
-	FiDb           Tu8
-	Fbusy          Tu8
-	F__ccgo_align3 [2]byte
-	F__ccgo8       uint8
-	FazInit        uintptr
+type Tsqlite3_index_constraint = struct {
+	FiColumn     int32
+	Fop          uint8
+	Fusable      uint8
+	FiTermOffset int32
+}
+
+type Tsqlite3_index_orderby = struct {
+	FiColumn     int32
+	Fdesc        uint8
+	F__ccgo_pad2 [3]byte
+}
+
+type TWalSegment = struct {
+	FiNext  int32
+	FaIndex uintptr
+	FaPgno  uintptr
+	FnEntry int32
+	FiZero  int32
+}
+
+type T_ht = struct {
+	Fcount uint32
+	Fchain uintptr
+}
+
+type Tsqlite3_index_constraint_usage = struct {
+	FargvIndex   int32
+	Fomit        uint8
+	F__ccgo_pad2 [3]byte
+}
+
+type TMemValue = struct {
+	F__ccgo_align [0]uint32
+	Fi            [0]Ti64
+	FnZero        [0]int32
+	FzPType       [0]uintptr
+	FpDef         [0]uintptr
+	Fr            float64
 }
 
 type Tp4union = struct {
@@ -151888,15 +151897,6 @@ type Tp4union = struct {
 	FpTab     [0]uintptr
 	FxAdvance [0]uintptr
 	Fi        int32
-}
-
-type TMemValue = struct {
-	F__ccgo_align [0]uint32
-	Fi            [0]Ti64
-	FnZero        [0]int32
-	FzPType       [0]uintptr
-	FpDef         [0]uintptr
-	Fr            float64
 }
 
 func __ccgo_fp(f interface{}) uintptr {
--- internal/testfixture/ccgo_linux_arm.go
+++ internal/testfixture/ccgo_linux_arm.go
@@ -1,4 +1,4 @@
-// Code generated for linux/arm by 'gcc -DSQLITE_THREADSAFE=0 -Dpread64=pread -Dpwrite64=pwrite -DNDEBUG -absolute-paths -keep-object-files -positions --libc modernc.org/libc --prefix-enumerator=_ --prefix-external=x_ --prefix-field=F --prefix-macro=m_ --prefix-static-internal=_ --prefix-static-none=_ --prefix-tagged-enum=_ --prefix-tagged-struct=T --prefix-tagged-union=T --prefix-typename=T --prefix-undefined=_ -extended-errors -I/tmp/libsqlite3/sqlite-amalgamation-3370200/ccgo -DLONGDOUBLE_TYPE=double -DSQLITE_THREADSAFE=0 -Dpread64=pread -Dpwrite64=pwrite -DNDEBUG -DSQLITE_OS_UNIX=1 -I. -I/tmp/libsqlite3/sqlite-src-3370200/src -I/tmp/libsqlite3/sqlite-src-3370200/ext/rtree -I/tmp/libsqlite3/sqlite-src-3370200/ext/icu -I/tmp/libsqlite3/sqlite-src-3370200/ext/fts3 -I/tmp/libsqlite3/sqlite-src-3370200/ext/async -I/tmp/libsqlite3/sqlite-src-3370200/ext/session -I/tmp/libsqlite3/sqlite-src-3370200/ext/userauth -D_HAVE_SQLITE_CONFIG_H -DBUILD_sqlite -DNDEBUG -I/usr/include/tcl8.6 -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION=1 -DSQLITE_ENABLE_MATH_FUNCTIONS -DSQLITE_HAVE_ZLIB=1 -DSQLITE_NO_SYNC=1 -DSQLITE_TEMP_STORE=1 -DSQLITE_TEST=1 -DSQLITE_CRASH_TEST=1 -DTCLSH_INIT_PROC=sqlite3TestInit -DSQLITE_SERVER=1 -DSQLITE_PRIVATE= -DSQLITE_CORE -DBUILD_sqlite -DSQLITE_SERIES_CONSTRAINT_VERIFY=1 -DSQLITE_DEFAULT_PAGE_SIZE=1024 -DSQLITE_ENABLE_STMTVTAB -DSQLITE_ENABLE_DBPAGE_VTAB -DSQLITE_ENABLE_BYTECODE_VTAB -DSQLITE_CKSUMVFS_STATIC -o testfixture.go /tmp/libsqlite3/sqlite-src-3370200/src/test1.c /tmp/libsqlite3/sqlite-src-3370200/src/test2.c /tmp/libsqlite3/sqlite-src-3370200/src/test3.c /tmp/libsqlite3/sqlite-src-3370200/src/test4.c /tmp/libsqlite3/sqlite-src-3370200/src/test5.c /tmp/libsqlite3/sqlite-src-3370200/src/test6.c /tmp/libsqlite3/sqlite-src-3370200/src/test7.c /tmp/libsqlite3/sqlite-src-3370200/src/test8.c /tmp/libsqlite3/sqlite-src-3370200/src/test9.c /tmp/libsqlite3/sqlite-src-3370200/src/test_autoext.c /tmp/libsqlite3/sqlite-src-3370200/src/test_async.c /tmp/libsqlite3/sqlite-src-3370200/src/test_backup.c /tmp/libsqlite3/sqlite-src-3370200/src/test_bestindex.c /tmp/libsqlite3/sqlite-src-3370200/src/test_blob.c /tmp/libsqlite3/sqlite-src-3370200/src/test_btree.c /tmp/libsqlite3/sqlite-src-3370200/src/test_config.c /tmp/libsqlite3/sqlite-src-3370200/src/test_delete.c /tmp/libsqlite3/sqlite-src-3370200/src/test_demovfs.c /tmp/libsqlite3/sqlite-src-3370200/src/test_devsym.c /tmp/libsqlite3/sqlite-src-3370200/src/test_fs.c /tmp/libsqlite3/sqlite-src-3370200/src/test_func.c /tmp/libsqlite3/sqlite-src-3370200/src/test_hexio.c /tmp/libsqlite3/sqlite-src-3370200/src/test_init.c /tmp/libsqlite3/sqlite-src-3370200/src/test_intarray.c /tmp/libsqlite3/sqlite-src-3370200/src/test_journal.c /tmp/libsqlite3/sqlite-src-3370200/src/test_malloc.c /tmp/libsqlite3/sqlite-src-3370200/src/test_md5.c /tmp/libsqlite3/sqlite-src-3370200/src/test_multiplex.c /tmp/libsqlite3/sqlite-src-3370200/src/test_mutex.c /tmp/libsqlite3/sqlite-src-3370200/src/test_onefile.c /tmp/libsqlite3/sqlite-src-3370200/src/test_osinst.c /tmp/libsqlite3/sqlite-src-3370200/src/test_pcache.c /tmp/libsqlite3/sqlite-src-3370200/src/test_quota.c /tmp/libsqlite3/sqlite-src-3370200/src/test_rtree.c /tmp/libsqlite3/sqlite-src-3370200/src/test_schema.c /tmp/libsqlite3/sqlite-src-3370200/src/test_server.c /tmp/libsqlite3/sqlite-src-3370200/src/test_superlock.c /tmp/libsqlite3/sqlite-src-3370200/src/test_syscall.c /tmp/libsqlite3/sqlite-src-3370200/src/test_tclsh.c /tmp/libsqlite3/sqlite-src-3370200/src/test_tclvar.c /tmp/libsqlite3/sqlite-src-3370200/src/test_thread.c /tmp/libsqlite3/sqlite-src-3370200/src/test_vdbecov.c /tmp/libsqlite3/sqlite-src-3370200/src/test_vfs.c /tmp/libsqlite3/sqlite-src-3370200/src/test_windirent.c /tmp/libsqlite3/sqlite-src-3370200/src/test_window.c /tmp/libsqlite3/sqlite-src-3370200/src/test_wsd.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts3/fts3_term.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts3/fts3_test.c /tmp/libsqlite3/sqlite-src-3370200/ext/session/test_session.c /tmp/libsqlite3/sqlite-src-3370200/ext/rbu/test_rbu.c /tmp/libsqlite3/sqlite-src-3370200/ext/expert/sqlite3expert.c /tmp/libsqlite3/sqlite-src-3370200/ext/expert/test_expert.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/amatch.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/appendvfs.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/carray.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/cksumvfs.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/closure.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/csv.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/decimal.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/eval.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/explain.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/fileio.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/fuzzer.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts5/fts5_tcl.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts5/fts5_test_mi.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts5/fts5_test_tok.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/ieee754.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/mmapwarm.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/nextchar.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/normalize.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/percentile.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/prefixes.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/regexp.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/remember.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/series.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/spellfix.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/totype.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/unionvtab.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/wholenumber.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/zipfile.c /tmp/libsqlite3/sqlite-src-3370200/ext/userauth/userauth.c /tmp/libsqlite3/sqlite-src-3370200/ext/rtree/test_rtreedoc.c /tmp/libsqlite3/sqlite-src-3370200/src/tclsqlite.c sqlite3.c -L/usr/lib/arm-linux-gnueabihf -ltcl8.6 -lm -lz', DO NOT EDIT.
+// Code generated for linux/arm by 'gcc -DSQLITE_THREADSAFE=0 -Dpread64=pread -Dpwrite64=pwrite -DNDEBUG -absolute-paths -keep-object-files -positions --libc modernc.org/libc --prefix-enumerator=_ --prefix-external=x_ --prefix-field=F --prefix-macro=m_ --prefix-static-internal=_ --prefix-static-none=_ --prefix-tagged-enum=_ --prefix-tagged-struct=T --prefix-tagged-union=T --prefix-typename=T --prefix-undefined=_ -extended-errors -ignore-unsupported-alignment -I/tmp/libsqlite3/sqlite-amalgamation-3370200/ccgo -DLONGDOUBLE_TYPE=double -DSQLITE_THREADSAFE=0 -Dpread64=pread -Dpwrite64=pwrite -DNDEBUG -DSQLITE_OS_UNIX=1 -I. -I/tmp/libsqlite3/sqlite-src-3370200/src -I/tmp/libsqlite3/sqlite-src-3370200/ext/rtree -I/tmp/libsqlite3/sqlite-src-3370200/ext/icu -I/tmp/libsqlite3/sqlite-src-3370200/ext/fts3 -I/tmp/libsqlite3/sqlite-src-3370200/ext/async -I/tmp/libsqlite3/sqlite-src-3370200/ext/session -I/tmp/libsqlite3/sqlite-src-3370200/ext/userauth -D_HAVE_SQLITE_CONFIG_H -DBUILD_sqlite -DNDEBUG -I/usr/include/tcl8.6 -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION=1 -DSQLITE_ENABLE_MATH_FUNCTIONS -DSQLITE_HAVE_ZLIB=1 -DSQLITE_NO_SYNC=1 -DSQLITE_TEMP_STORE=1 -DSQLITE_TEST=1 -DSQLITE_CRASH_TEST=1 -DTCLSH_INIT_PROC=sqlite3TestInit -DSQLITE_SERVER=1 -DSQLITE_PRIVATE= -DSQLITE_CORE -DBUILD_sqlite -DSQLITE_SERIES_CONSTRAINT_VERIFY=1 -DSQLITE_DEFAULT_PAGE_SIZE=1024 -DSQLITE_ENABLE_STMTVTAB -DSQLITE_ENABLE_DBPAGE_VTAB -DSQLITE_ENABLE_BYTECODE_VTAB -DSQLITE_CKSUMVFS_STATIC -o testfixture.go /tmp/libsqlite3/sqlite-src-3370200/src/test1.c /tmp/libsqlite3/sqlite-src-3370200/src/test2.c /tmp/libsqlite3/sqlite-src-3370200/src/test3.c /tmp/libsqlite3/sqlite-src-3370200/src/test4.c /tmp/libsqlite3/sqlite-src-3370200/src/test5.c /tmp/libsqlite3/sqlite-src-3370200/src/test6.c /tmp/libsqlite3/sqlite-src-3370200/src/test7.c /tmp/libsqlite3/sqlite-src-3370200/src/test8.c /tmp/libsqlite3/sqlite-src-3370200/src/test9.c /tmp/libsqlite3/sqlite-src-3370200/src/test_autoext.c /tmp/libsqlite3/sqlite-src-3370200/src/test_async.c /tmp/libsqlite3/sqlite-src-3370200/src/test_backup.c /tmp/libsqlite3/sqlite-src-3370200/src/test_bestindex.c /tmp/libsqlite3/sqlite-src-3370200/src/test_blob.c /tmp/libsqlite3/sqlite-src-3370200/src/test_btree.c /tmp/libsqlite3/sqlite-src-3370200/src/test_config.c /tmp/libsqlite3/sqlite-src-3370200/src/test_delete.c /tmp/libsqlite3/sqlite-src-3370200/src/test_demovfs.c /tmp/libsqlite3/sqlite-src-3370200/src/test_devsym.c /tmp/libsqlite3/sqlite-src-3370200/src/test_fs.c /tmp/libsqlite3/sqlite-src-3370200/src/test_func.c /tmp/libsqlite3/sqlite-src-3370200/src/test_hexio.c /tmp/libsqlite3/sqlite-src-3370200/src/test_init.c /tmp/libsqlite3/sqlite-src-3370200/src/test_intarray.c /tmp/libsqlite3/sqlite-src-3370200/src/test_journal.c /tmp/libsqlite3/sqlite-src-3370200/src/test_malloc.c /tmp/libsqlite3/sqlite-src-3370200/src/test_md5.c /tmp/libsqlite3/sqlite-src-3370200/src/test_multiplex.c /tmp/libsqlite3/sqlite-src-3370200/src/test_mutex.c /tmp/libsqlite3/sqlite-src-3370200/src/test_onefile.c /tmp/libsqlite3/sqlite-src-3370200/src/test_osinst.c /tmp/libsqlite3/sqlite-src-3370200/src/test_pcache.c /tmp/libsqlite3/sqlite-src-3370200/src/test_quota.c /tmp/libsqlite3/sqlite-src-3370200/src/test_rtree.c /tmp/libsqlite3/sqlite-src-3370200/src/test_schema.c /tmp/libsqlite3/sqlite-src-3370200/src/test_server.c /tmp/libsqlite3/sqlite-src-3370200/src/test_superlock.c /tmp/libsqlite3/sqlite-src-3370200/src/test_syscall.c /tmp/libsqlite3/sqlite-src-3370200/src/test_tclsh.c /tmp/libsqlite3/sqlite-src-3370200/src/test_tclvar.c /tmp/libsqlite3/sqlite-src-3370200/src/test_thread.c /tmp/libsqlite3/sqlite-src-3370200/src/test_vdbecov.c /tmp/libsqlite3/sqlite-src-3370200/src/test_vfs.c /tmp/libsqlite3/sqlite-src-3370200/src/test_windirent.c /tmp/libsqlite3/sqlite-src-3370200/src/test_window.c /tmp/libsqlite3/sqlite-src-3370200/src/test_wsd.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts3/fts3_term.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts3/fts3_test.c /tmp/libsqlite3/sqlite-src-3370200/ext/session/test_session.c /tmp/libsqlite3/sqlite-src-3370200/ext/rbu/test_rbu.c /tmp/libsqlite3/sqlite-src-3370200/ext/expert/sqlite3expert.c /tmp/libsqlite3/sqlite-src-3370200/ext/expert/test_expert.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/amatch.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/appendvfs.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/carray.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/cksumvfs.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/closure.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/csv.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/decimal.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/eval.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/explain.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/fileio.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/fuzzer.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts5/fts5_tcl.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts5/fts5_test_mi.c /tmp/libsqlite3/sqlite-src-3370200/ext/fts5/fts5_test_tok.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/ieee754.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/mmapwarm.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/nextchar.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/normalize.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/percentile.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/prefixes.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/regexp.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/remember.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/series.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/spellfix.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/totype.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/unionvtab.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/wholenumber.c /tmp/libsqlite3/sqlite-src-3370200/ext/misc/zipfile.c /tmp/libsqlite3/sqlite-src-3370200/ext/userauth/userauth.c /tmp/libsqlite3/sqlite-src-3370200/ext/rtree/test_rtreedoc.c /tmp/libsqlite3/sqlite-src-3370200/src/tclsqlite.c sqlite3.c -L/usr/lib/arm-linux-gnueabihf -ltcl8.6 -lm -lz', DO NOT EDIT.
 
 //go:build linux && arm
 // +build linux,arm
@@ -26720,18 +26720,18 @@ func init() {
 }
 
 // -:
-type Tsqlite3_index_constraint_usage = struct {
-	FargvIndex   int32
-	Fomit        uint8
-	F__ccgo_pad2 [3]byte
-}
-
-// -:
 type Tsqlite3_index_constraint = struct {
 	FiColumn     int32
 	Fop          uint8
 	Fusable      uint8
 	FiTermOffset int32
+}
+
+// -:
+type Tsqlite3_index_constraint_usage = struct {
+	FargvIndex   int32
+	Fomit        uint8
+	F__ccgo_pad2 [3]byte
 }
 
 // -:
@@ -306683,18 +306683,18 @@ func x_sqlite3_sourceid(tls *libc.TLS) (r uintptr) {
 }
 
 // -:
-type TIdList_item = struct {
-	FzName uintptr
-	Fidx   int32
+type T_ht = struct {
+	Fcount uint32
+	Fchain uintptr
 }
 
 // -:
-type TWalSegment = struct {
-	FiNext  int32
-	FaIndex uintptr
-	FaPgno  uintptr
-	FnEntry int32
-	FiZero  int32
+type TAggInfo_func = struct {
+	FpFExpr    uintptr
+	FpFunc     uintptr
+	FiMem      int32
+	FiDistinct int32
+	FiDistAddr int32
 }
 
 // -:
@@ -306718,18 +306718,18 @@ type TInLoop = struct {
 }
 
 // -:
-type TAggInfo_func = struct {
-	FpFExpr    uintptr
-	FpFunc     uintptr
-	FiMem      int32
-	FiDistinct int32
-	FiDistAddr int32
+type TIdList_item = struct {
+	FzName uintptr
+	Fidx   int32
 }
 
 // -:
-type T_ht = struct {
-	Fcount uint32
-	Fchain uintptr
+type TWalSegment = struct {
+	FiNext  int32
+	FaIndex uintptr
+	FaPgno  uintptr
+	FnEntry int32
+	FiZero  int32
 }
 
 func __ccgo_fp(f interface{}) uintptr {
