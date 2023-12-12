a5fb15a11fe43d55a65fedfc5ff5d5e8b105dbe6
Jan Mercl <0xjnml@gmail.com>
2023-12-12 19:30:08 +0100

    ppc64le: auto generate

--- internal/testfixture/ccgo_linux_ppc64le.go
+++ internal/testfixture/ccgo_linux_ppc64le.go
@@ -19415,9 +19415,13 @@ var _longdouble_size = int32(8)
 var _query_plan = __ccgo_ts + 8987
 
 // -:
-type TsColMap = struct {
-	FiFrom int32
-	FzCol  uintptr
+type Tsqlite3InitInfo = struct {
+	FnewTnum       TPgno
+	FiDb           Tu8
+	Fbusy          Tu8
+	F__ccgo_align3 [2]byte
+	F__ccgo8       uint8
+	FazInit        uintptr
 }
 
 // -:
@@ -19438,13 +19442,9 @@ type TExprList_item = struct {
 }
 
 // -:
-type Tsqlite3InitInfo = struct {
-	FnewTnum       TPgno
-	FiDb           Tu8
-	Fbusy          Tu8
-	F__ccgo_align3 [2]byte
-	F__ccgo8       uint8
-	FazInit        uintptr
+type TsColMap = struct {
+	FiFrom int32
+	FzCol  uintptr
 }
 
 // -:
@@ -26654,10 +26654,11 @@ func init() {
 }
 
 // -:
-type Tsqlite3_index_orderby = struct {
+type Tsqlite3_index_constraint = struct {
 	FiColumn     int32
-	Fdesc        uint8
-	F__ccgo_pad2 [3]byte
+	Fop          uint8
+	Fusable      uint8
+	FiTermOffset int32
 }
 
 // -:
@@ -26668,11 +26669,10 @@ type Tsqlite3_index_constraint_usage = struct {
 }
 
 // -:
-type Tsqlite3_index_constraint = struct {
+type Tsqlite3_index_orderby = struct {
 	FiColumn     int32
-	Fop          uint8
-	Fusable      uint8
-	FiTermOffset int32
+	Fdesc        uint8
+	F__ccgo_pad2 [3]byte
 }
 
 /*
@@ -306833,10 +306833,35 @@ func x_sqlite3_sourceid(tls *libc.TLS) (r uintptr) {
 }
 
 // -:
+type TWalSegment = struct {
+	FiNext  int32
+	FaIndex uintptr
+	FaPgno  uintptr
+	FnEntry int32
+	FiZero  int32
+}
+
+// -:
 type TIdList_item = struct {
 	FzName       uintptr
 	Fidx         int32
 	F__ccgo_pad2 [4]byte
+}
+
+// -:
+type TInLoop = struct {
+	FiCur        int32
+	FaddrInTop   int32
+	FiBase       int32
+	FnPrefix     int32
+	FeEndLoopOp  Tu8
+	F__ccgo_pad5 [3]byte
+}
+
+// -:
+type T_ht = struct {
+	Fcount uint32
+	Fchain uintptr
 }
 
 // -:
@@ -306858,31 +306883,6 @@ type TAggInfo_func = struct {
 	FiDistinct   int32
 	FiDistAddr   int32
 	F__ccgo_pad5 [4]byte
-}
-
-// -:
-type T_ht = struct {
-	Fcount uint32
-	Fchain uintptr
-}
-
-// -:
-type TInLoop = struct {
-	FiCur        int32
-	FaddrInTop   int32
-	FiBase       int32
-	FnPrefix     int32
-	FeEndLoopOp  Tu8
-	F__ccgo_pad5 [3]byte
-}
-
-// -:
-type TWalSegment = struct {
-	FiNext  int32
-	FaIndex uintptr
-	FaPgno  uintptr
-	FnEntry int32
-	FiZero  int32
 }
 
 func __ccgo_fp(f interface{}) uintptr {
