1eeb654b646a15c0ed7420cc70506d9d269abf75
Jan Mercl <0xjnml@gmail.com>
2024-01-09 03:46:52 +0100

    darwin-m1: auto generate

--- ccgo_darwin_arm64.go
+++ ccgo_darwin_arm64.go
@@ -150109,11 +150109,6 @@ func Xsqlite3_sourceid(tls *libc.TLS) (r uintptr) {
 	return __ccgo_ts + 23744
 }
 
-type TsColMap = struct {
-	FiFrom int32
-	FzCol  uintptr
-}
-
 type Tsqlite3_index_constraint = struct {
 	FiColumn     int32
 	Fop          uint8
@@ -150127,6 +150122,17 @@ type Tsqlite3_index_orderby = struct {
 	F__ccgo_pad2 [3]byte
 }
 
+type TsColMap = struct {
+	FiFrom int32
+	FzCol  uintptr
+}
+
+type TIdList_item = struct {
+	FzName       uintptr
+	Fidx         int32
+	F__ccgo_pad2 [4]byte
+}
+
 type TAggInfo_col = struct {
 	FpTab          uintptr
 	FpCExpr        uintptr
@@ -150137,27 +150143,10 @@ type TAggInfo_col = struct {
 	F__ccgo_pad6   [4]byte
 }
 
-type TAggInfo_func = struct {
-	FpFExpr      uintptr
-	FpFunc       uintptr
-	FiMem        int32
-	FiDistinct   int32
-	FiDistAddr   int32
-	F__ccgo_pad5 [4]byte
-}
-
 type Tsqlite3_index_constraint_usage = struct {
 	FargvIndex   int32
 	Fomit        uint8
 	F__ccgo_pad2 [3]byte
-}
-
-type TWalSegment = struct {
-	FiNext  int32
-	FaIndex uintptr
-	FaPgno  uintptr
-	FnEntry int32
-	FiZero  int32
 }
 
 type TInLoop = struct {
@@ -150169,10 +150158,12 @@ type TInLoop = struct {
 	F__ccgo_pad5 [3]byte
 }
 
-type TIdList_item = struct {
-	FzName       uintptr
-	Fidx         int32
-	F__ccgo_pad2 [4]byte
+type TWalSegment = struct {
+	FiNext  int32
+	FaIndex uintptr
+	FaPgno  uintptr
+	FnEntry int32
+	FiZero  int32
 }
 
 type T_ht = struct {
@@ -150189,12 +150180,13 @@ type Tsqlite3InitInfo = struct {
 	FazInit        uintptr
 }
 
-type TMemValue = struct {
-	Fi      [0]int64
-	FnZero  [0]int32
-	FzPType [0]uintptr
-	FpDef   [0]uintptr
-	Fr      float64
+type TAggInfo_func = struct {
+	FpFExpr      uintptr
+	FpFunc       uintptr
+	FiMem        int32
+	FiDistinct   int32
+	FiDistAddr   int32
+	F__ccgo_pad5 [4]byte
 }
 
 type Tp4union = struct {
@@ -150214,6 +150206,14 @@ type Tp4union = struct {
 	FxAdvance     [0]uintptr
 	Fi            int32
 	F__ccgo_pad15 [4]byte
+}
+
+type TMemValue = struct {
+	Fi      [0]int64
+	FnZero  [0]int32
+	FzPType [0]uintptr
+	FpDef   [0]uintptr
+	Fr      float64
 }
 
 func __ccgo_fp(f interface{}) uintptr {
