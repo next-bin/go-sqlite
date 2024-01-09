db8f0c253655ce1e57a50740870f9a93e66c85c4
Jan Mercl <0xjnml@gmail.com>
2024-01-09 20:57:16 +0100

    s390x: auto generate

--- internal/testfixture/ccgo_linux_s390x.go
+++ internal/testfixture/ccgo_linux_s390x.go
@@ -17177,7 +17177,7 @@ func _test_getrusage(tls *libc.TLS, clientData uintptr, interp uintptr, objc int
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test1.c:7187:3:
 	libc.Xmemset(tls, bp+1024, 0, uint64(144))
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test1.c:7188:3:
-	libc.Xgetrusage(tls, int32(_RUSAGE_SELF), bp+1024)
+	libc.Xgetrusage(tls, _RUSAGE_SELF, bp+1024)
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test1.c:7190:3:
 	x_sqlite3_snprintf(tls, int32(1024), bp, __ccgo_ts+3735, libc.VaList(bp+1176, int32((*(*Trusage)(unsafe.Pointer(bp + 1024))).Fru_utime.Ftv_sec), int32((*(*Trusage)(unsafe.Pointer(bp + 1024))).Fru_utime.Ftv_usec), int32((*(*Trusage)(unsafe.Pointer(bp + 1024))).Fru_stime.Ftv_sec), int32((*(*Trusage)(unsafe.Pointer(bp + 1024))).Fru_stime.Ftv_usec), int32((*(*Trusage)(unsafe.Pointer(bp + 1024))).F__ccgo6_64.Fru_minflt), int32((*(*Trusage)(unsafe.Pointer(bp + 1024))).F__ccgo7_72.Fru_majflt)))
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test1.c:7196:3:
@@ -19414,16 +19414,6 @@ var _longdouble_size = int32(8)
 var _query_plan = __ccgo_ts + 8987
 
 // -:
-type Tsqlite3InitInfo = struct {
-	FnewTnum       TPgno
-	FiDb           Tu8
-	Fbusy          Tu8
-	F__ccgo_align3 [2]byte
-	F__ccgo8       uint8
-	FazInit        uintptr
-}
-
-// -:
 type TExprList_item = struct {
 	FpExpr         uintptr
 	FzEName        uintptr
@@ -19447,12 +19437,13 @@ type TsColMap = struct {
 }
 
 // -:
-type TMemValue = struct {
-	Fi      [0]Ti64
-	FnZero  [0]int32
-	FzPType [0]uintptr
-	FpDef   [0]uintptr
-	Fr      float64
+type Tsqlite3InitInfo = struct {
+	FnewTnum       TPgno
+	FiDb           Tu8
+	Fbusy          Tu8
+	F__ccgo_align3 [2]byte
+	F__ccgo8       uint8
+	FazInit        uintptr
 }
 
 // -:
@@ -19473,6 +19464,15 @@ type Tp4union = struct {
 	FxAdvance     [0]uintptr
 	Fi            int32
 	F__ccgo_pad15 [4]byte
+}
+
+// -:
+type TMemValue = struct {
+	Fi      [0]Ti64
+	FnZero  [0]int32
+	FzPType [0]uintptr
+	FpDef   [0]uintptr
+	Fr      float64
 }
 
 // /tmp/libsqlite3/sqlite-src-3370200/src/sqliteInt.h:2018:9:
@@ -26661,16 +26661,16 @@ type Tsqlite3_index_constraint = struct {
 }
 
 // -:
-type Tsqlite3_index_orderby = struct {
-	FiColumn     int32
-	Fdesc        uint8
+type Tsqlite3_index_constraint_usage = struct {
+	FargvIndex   int32
+	Fomit        uint8
 	F__ccgo_pad2 [3]byte
 }
 
 // -:
-type Tsqlite3_index_constraint_usage = struct {
-	FargvIndex   int32
-	Fomit        uint8
+type Tsqlite3_index_orderby = struct {
+	FiColumn     int32
+	Fdesc        uint8
 	F__ccgo_pad2 [3]byte
 }
 
@@ -55865,11 +55865,11 @@ func x_sqlite3TestInit(tls *libc.TLS, interp uintptr) (r uintptr) {
 	/* Since the primary use case for this binary is testing of SQLite,
 	 ** be sure to generate core files if we crash */
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test_tclsh.c:118:5:
-	libc.Xgetrlimit(tls, int32(_RLIMIT_CORE), bp+64)
+	libc.Xgetrlimit(tls, _RLIMIT_CORE, bp+64)
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test_tclsh.c:119:5:
 	(*(*Trlimit)(unsafe.Pointer(bp + 64))).Frlim_cur = (*(*Trlimit)(unsafe.Pointer(bp + 64))).Frlim_max
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test_tclsh.c:120:5:
-	libc.Xsetrlimit(tls, int32(_RLIMIT_CORE), bp+64)
+	libc.Xsetrlimit(tls, _RLIMIT_CORE, bp+64)
 	// /tmp/libsqlite3/sqlite-src-3370200/src/test_tclsh.c:124:3:
 	if libtcl8_6.XTcl_GetCommandInfo(tls, interp, __ccgo_ts+21815, bp) == 0 {
 		// /tmp/libsqlite3/sqlite-src-3370200/src/test_tclsh.c:125:5:
@@ -124459,7 +124459,7 @@ func _unixDeviceCharacteristics(tls *libc.TLS, id uintptr) (r int32) {
 func _unixGetpagesize(tls *libc.TLS) (r int32) {
 	// /tmp/libsqlite3/sqlite-src-3370200/sqlite3.c:38472:33:
 	// /tmp/libsqlite3/sqlite-src-3370200/sqlite3.c:38478:3:
-	return int32(libc.Xsysconf(tls, int32(__SC_PAGESIZE)))
+	return int32(libc.Xsysconf(tls, __SC_PAGESIZE))
 }
 
 /*
@@ -306752,6 +306752,25 @@ func x_sqlite3_sourceid(tls *libc.TLS) (r uintptr) {
 }
 
 // -:
+type TWalSegment = struct {
+	FiNext  int32
+	FaIndex uintptr
+	FaPgno  uintptr
+	FnEntry int32
+	FiZero  int32
+}
+
+// -:
+type TAggInfo_func = struct {
+	FpFExpr      uintptr
+	FpFunc       uintptr
+	FiMem        int32
+	FiDistinct   int32
+	FiDistAddr   int32
+	F__ccgo_pad5 [4]byte
+}
+
+// -:
 type TInLoop = struct {
 	FiCur        int32
 	FaddrInTop   int32
@@ -306759,12 +306778,6 @@ type TInLoop = struct {
 	FnPrefix     int32
 	FeEndLoopOp  Tu8
 	F__ccgo_pad5 [3]byte
-}
-
-// -:
-type T_ht = struct {
-	Fcount uint32
-	Fchain uintptr
 }
 
 // -:
@@ -306779,16 +306792,6 @@ type TAggInfo_col = struct {
 }
 
 // -:
-type TAggInfo_func = struct {
-	FpFExpr      uintptr
-	FpFunc       uintptr
-	FiMem        int32
-	FiDistinct   int32
-	FiDistAddr   int32
-	F__ccgo_pad5 [4]byte
-}
-
-// -:
 type TIdList_item = struct {
 	FzName       uintptr
 	Fidx         int32
@@ -306796,12 +306799,9 @@ type TIdList_item = struct {
 }
 
 // -:
-type TWalSegment = struct {
-	FiNext  int32
-	FaIndex uintptr
-	FaPgno  uintptr
-	FnEntry int32
-	FiZero  int32
+type T_ht = struct {
+	Fcount uint32
+	Fchain uintptr
 }
 
 func __ccgo_fp(f interface{}) uintptr {
