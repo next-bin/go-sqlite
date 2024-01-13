1e93a4cdd47446cb4c0cad02f5ac427433d9d023
Jan Mercl <0xjnml@gmail.com>
2024-01-13 22:30:33 +0100

    freebsd64: auto generate

--- ccgo_freebsd_amd64.go
+++ ccgo_freebsd_amd64.go
@@ -514,20 +514,19 @@ type Tz_stream_s = Tz_stream
 type Tz_streamp = uintptr
 
 type Tgz_header = struct {
-	Ftext         int32
-	Ftime         TuLong
-	Fxflags       int32
-	Fos           int32
-	Fextra        uintptr
-	Fextra_len    TuInt
-	Fextra_max    TuInt
-	Fname         uintptr
-	Fname_max     TuInt
-	Fcomment      uintptr
-	Fcomm_max     TuInt
-	Fhcrc         int32
-	Fdone         int32
-	F__ccgo_pad13 [4]byte
+	Ftext      int32
+	Ftime      TuLong
+	Fxflags    int32
+	Fos        int32
+	Fextra     uintptr
+	Fextra_len TuInt
+	Fextra_max TuInt
+	Fname      uintptr
+	Fname_max  TuInt
+	Fcomment   uintptr
+	Fcomm_max  TuInt
+	Fhcrc      int32
+	Fdone      int32
 }
 
 type Tgz_header_s = Tgz_header
@@ -8762,42 +8761,41 @@ const _MEM = 16210
 const _SYNC = 16211
 
 type Tinflate_state = struct {
-	Fstrm         Tz_streamp
-	Fmode         Tinflate_mode
-	Flast         int32
-	Fwrap         int32
-	Fhavedict     int32
-	Fflags        int32
-	Fdmax         uint32
-	Fcheck        uint64
-	Ftotal        uint64
-	Fhead         Tgz_headerp
-	Fwbits        uint32
-	Fwsize        uint32
-	Fwhave        uint32
-	Fwnext        uint32
-	Fwindow       uintptr
-	Fhold         uint64
-	Fbits         uint32
-	Flength       uint32
-	Foffset       uint32
-	Fextra        uint32
-	Flencode      uintptr
-	Fdistcode     uintptr
-	Flenbits      uint32
-	Fdistbits     uint32
-	Fncode        uint32
-	Fnlen         uint32
-	Fndist        uint32
-	Fhave         uint32
-	Fnext         uintptr
-	Flens         [320]uint16
-	Fwork         [288]uint16
-	Fcodes        [1444]Tcode
-	Fsane         int32
-	Fback         int32
-	Fwas          uint32
-	F__ccgo_pad35 [4]byte
+	Fstrm     Tz_streamp
+	Fmode     Tinflate_mode
+	Flast     int32
+	Fwrap     int32
+	Fhavedict int32
+	Fflags    int32
+	Fdmax     uint32
+	Fcheck    uint64
+	Ftotal    uint64
+	Fhead     Tgz_headerp
+	Fwbits    uint32
+	Fwsize    uint32
+	Fwhave    uint32
+	Fwnext    uint32
+	Fwindow   uintptr
+	Fhold     uint64
+	Fbits     uint32
+	Flength   uint32
+	Foffset   uint32
+	Fextra    uint32
+	Flencode  uintptr
+	Fdistcode uintptr
+	Flenbits  uint32
+	Fdistbits uint32
+	Fncode    uint32
+	Fnlen     uint32
+	Fndist    uint32
+	Fhave     uint32
+	Fnext     uintptr
+	Flens     [320]uint16
+	Fwork     [288]uint16
+	Fcodes    [1444]Tcode
+	Fsane     int32
+	Fback     int32
+	Fwas      uint32
 }
 
 // C documentation
@@ -21685,9 +21683,8 @@ func x__tr_tally(tls *libc.TLS, s uintptr, dist uint32, lc uint32) (r int32) {
 type Tfpos_t = int64
 
 type t__sbuf = struct {
-	F_base       uintptr
-	F_size       int32
-	F__ccgo_pad2 [4]byte
+	F_base uintptr
+	F_size int32
 }
 
 type t__sFILE = struct {
@@ -21717,7 +21714,6 @@ type t__sFILE = struct {
 	F_orientation int32
 	F_mbstate     t__mbstate_t
 	F_flags2      int32
-	F__ccgo_pad26 [4]byte
 }
 
 type TFILE = struct {
@@ -21747,7 +21743,6 @@ type TFILE = struct {
 	F_orientation int32
 	F_mbstate     t__mbstate_t
 	F_flags2      int32
-	F__ccgo_pad26 [4]byte
 }
 
 type Tcookie_io_functions_t = struct {
@@ -21758,13 +21753,12 @@ type Tcookie_io_functions_t = struct {
 }
 
 type Tflock = struct {
-	Fl_start     Toff_t
-	Fl_len       Toff_t
-	Fl_pid       Tpid_t
-	Fl_type      int16
-	Fl_whence    int16
-	Fl_sysid     int32
-	F__ccgo_pad6 [4]byte
+	Fl_start  Toff_t
+	Fl_len    Toff_t
+	Fl_pid    Tpid_t
+	Fl_type   int16
+	Fl_whence int16
+	Fl_sysid  int32
 }
 
 type t__oflock = struct {
