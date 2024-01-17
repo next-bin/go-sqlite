13de92dc1134503d2cbf8c3e26a00c3127ed7fdd
Jan Mercl <0xjnml@gmail.com>
2024-01-17 03:20:14 +0100

    temp disable windows

--- generator.go
+++ generator.go
@@ -33,7 +33,7 @@ var (
 	target = fmt.Sprintf("%s/%s", goos, goarch)
 	sed    = "sed"
 	j      = fmt.Sprint(runtime.GOMAXPROCS(-1))
-	win    = os.Getenv("GO_GENERATE_WIN") == "1"
+	win    = false //TODO hangs on nuc64: os.Getenv("GO_GENERATE_WIN") == "1"
 )
 
 // origin returns caller's short position, skipping skip frames.
