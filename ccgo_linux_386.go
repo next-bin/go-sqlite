d6740d8f3b57cc55bd0edfa8e71912ccd520875d
Jan Mercl <0xjnml@gmail.com>
2024-01-24 15:46:00 +0100

    e5-1650: auto generate

--- ccgo_linux_386.go
+++ ccgo_linux_386.go
@@ -151894,9 +151894,9 @@ func Xsqlite3_sourceid(tls *libc.TLS) (r uintptr) {
 	return __ccgo_ts + 23518
 }
 
-type TIdList_item = struct {
-	FzName uintptr
-	Fidx   int32
+type Tsqlite3_index_constraint_usage = struct {
+	FargvIndex int32
+	Fomit      uint8
 }
 
 type TAggInfo_func = struct {
@@ -151907,17 +151907,34 @@ type TAggInfo_func = struct {
 	FiDistAddr int32
 }
 
+type TsColMap = struct {
+	FiFrom int32
+	FzCol  uintptr
+}
+
+type T_ht = struct {
+	Fcount uint32
+	Fchain uintptr
+}
+
+type TIdList_item = struct {
+	FzName uintptr
+	Fidx   int32
+}
+
+type Tsqlite3_index_constraint = struct {
+	FiColumn     int32
+	Fop          uint8
+	Fusable      uint8
+	FiTermOffset int32
+}
+
 type TInLoop = struct {
 	FiCur       int32
 	FaddrInTop  int32
 	FiBase      int32
 	FnPrefix    int32
 	FeEndLoopOp Tu8
-}
-
-type TsColMap = struct {
-	FiFrom int32
-	FzCol  uintptr
 }
 
 type Tsqlite3InitInfo = struct {
@@ -151927,6 +151944,14 @@ type Tsqlite3InitInfo = struct {
 	F__ccgo_align3 [2]byte
 	F__ccgo8       uint8
 	FazInit        uintptr
+}
+
+type TWalSegment = struct {
+	FiNext  int32
+	FaIndex uintptr
+	FaPgno  uintptr
+	FnEntry int32
+	FiZero  int32
 }
 
 type TAggInfo_col = struct {
@@ -151941,31 +151966,6 @@ type TAggInfo_col = struct {
 type Tsqlite3_index_orderby = struct {
 	FiColumn int32
 	Fdesc    uint8
-}
-
-type Tsqlite3_index_constraint_usage = struct {
-	FargvIndex int32
-	Fomit      uint8
-}
-
-type T_ht = struct {
-	Fcount uint32
-	Fchain uintptr
-}
-
-type Tsqlite3_index_constraint = struct {
-	FiColumn     int32
-	Fop          uint8
-	Fusable      uint8
-	FiTermOffset int32
-}
-
-type TWalSegment = struct {
-	FiNext  int32
-	FaIndex uintptr
-	FaPgno  uintptr
-	FnEntry int32
-	FiZero  int32
 }
 
 type TMemValue = struct {
