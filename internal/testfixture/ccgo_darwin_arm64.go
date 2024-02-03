94c650166730b90c82ef6ba6a2345e54238cafac
Jan Mercl <0xjnml@gmail.com>
2024-02-03 17:49:10 +0100

    darwin-m1: auto generate

--- internal/testfixture/ccgo_darwin_arm64.go
+++ internal/testfixture/ccgo_darwin_arm64.go
@@ -15043,27 +15043,6 @@ func _test_autovacuum_pages(tls *libc.TLS, clientData uintptr, interp uintptr, o
 //	** Return a guess at the number of available cores available on the
 //	** processor on which this process is running.
 //	*/
-func _guess_number_of_cores(tls *libc.TLS, clientData uintptr, interp uintptr, objc int32, objv uintptr) (r int32) {
-	bp := tls.Alloc(32)
-	defer tls.Free(32)
-	var _ /* len at bp+16 */ Tsize_t
-	var _ /* nCore at bp+0 */ uint32
-	var _ /* nm at bp+8 */ [2]int32
-	*(*uint32)(unsafe.Pointer(bp)) = uint32(1)
-	*(*Tsize_t)(unsafe.Pointer(bp + 16)) = uint64(4)
-	(*(*[2]int32)(unsafe.Pointer(bp + 8)))[0] = int32(m_CTL_HW)
-	(*(*[2]int32)(unsafe.Pointer(bp + 8)))[int32(1)] = int32(m_HW_AVAILCPU)
-	_sysctl(tls, bp+8, uint32(2), bp, bp+16, libc.UintptrFromInt32(0), uint64(0))
-	if *(*uint32)(unsafe.Pointer(bp)) < uint32(1) {
-		(*(*[2]int32)(unsafe.Pointer(bp + 8)))[int32(1)] = int32(m_HW_NCPU)
-		_sysctl(tls, bp+8, uint32(2), bp, bp+16, libc.UintptrFromInt32(0), uint64(0))
-	}
-	if *(*uint32)(unsafe.Pointer(bp)) <= uint32(0) {
-		*(*uint32)(unsafe.Pointer(bp)) = uint32(1)
-	}
-	libtcl8_6.XTcl_SetObjResult(tls, interp, libtcl8_6.XTcl_NewIntObj(tls, int32(*(*uint32)(unsafe.Pointer(bp)))))
-	return m_SQLITE_OK
-}
 
 // C documentation
 //
