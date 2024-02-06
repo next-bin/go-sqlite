4edc0fffa0a957d566f2b76b7c9ec69f35429dcb
Jan Mercl <0xjnml@gmail.com>
2024-02-06 20:51:38 +0100

    ppc64le: auto generate

--- mptest/ccgo_linux_ppc64le.go
+++ mptest/ccgo_linux_ppc64le.go
@@ -1,4 +1,4 @@
-// Code generated for linux/ppc64le by 'generator -o mptest/ccgo_linux_ppc64le.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/mptest/mptest.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/ppc64le by 'generator -I /tmp/libsqlite3/sqlite-src-3450100 -ignore-unsupported-alignment -o mptest/ccgo_linux_ppc64le.go /tmp/libsqlite3/sqlite-src-3450100/mptest/mptest.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && ppc64le
 // +build linux,ppc64le
@@ -3108,7 +3108,7 @@ func main1(tls *libc.TLS, argc int32, argv uintptr) (r int32) {
 	if strglob(tls, __ccgo_ts+2410, g.zDbFile) != 0 {
 		usage(tls, *(*uintptr)(unsafe.Pointer(argv)))
 	}
-	if libc.Xstrcmp(tls, libsqlite3.Xsqlite3_sourceid(tls), __ccgo_ts+2417) != 0 {
+	if libc.Bool(0 != 0) && libc.Xstrcmp(tls, libsqlite3.Xsqlite3_sourceid(tls), __ccgo_ts+2417) != 0 {
 		libc.Xfprintf(tls, libc.Xstderr, __ccgo_ts+2502, libc.VaList(bp+40, libsqlite3.Xsqlite3_sourceid(tls), __ccgo_ts+2417))
 		libc.Xexit(tls, int32(1))
 	}
--- speedtest1/ccgo_linux_ppc64le.go
+++ speedtest1/ccgo_linux_ppc64le.go
@@ -1,4 +1,4 @@
-// Code generated for linux/ppc64le by 'generator -o speedtest1/ccgo_linux_ppc64le.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/ppc64le by 'generator -ignore-unsupported-alignment -o speedtest1/ccgo_linux_ppc64le.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && ppc64le
 // +build linux,ppc64le
