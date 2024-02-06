bbd9788007972be3da77c81ab782538702851ac1
Jan Mercl <0xjnml@gmail.com>
2024-02-06 21:17:45 +0100

    riscv64: auto generate

--- speedtest1/ccgo_linux_riscv64.go
+++ speedtest1/ccgo_linux_riscv64.go
@@ -1,4 +1,4 @@
-// Code generated for linux/riscv64 by 'generator -o speedtest1/ccgo_linux_riscv64.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/riscv64 by 'generator -ignore-unsupported-alignment -o speedtest1/ccgo_linux_riscv64.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && riscv64
 // +build linux,riscv64
