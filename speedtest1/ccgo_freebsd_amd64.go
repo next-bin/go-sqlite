d41814d66e1931f6dd618992062ce8419a11920b
Jan Mercl <0xjnml@gmail.com>
2024-02-06 20:42:58 +0100

    freebsd64: auto generate

--- speedtest1/ccgo_freebsd_amd64.go
+++ speedtest1/ccgo_freebsd_amd64.go
@@ -1,4 +1,4 @@
-// Code generated for freebsd/amd64 by 'generator -o speedtest1/ccgo_freebsd_amd64.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for freebsd/amd64 by 'generator -ignore-unsupported-alignment -o speedtest1/ccgo_freebsd_amd64.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build freebsd && amd64
 // +build freebsd,amd64
