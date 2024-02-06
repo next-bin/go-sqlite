3e5dbdc2b02d1125f71b8a74a45743c5868215c6
Jan Mercl <0xjnml@gmail.com>
2024-02-06 20:35:26 +0100

    nuc64: auto generate

--- speedtest1/ccgo_linux_amd64.go
+++ speedtest1/ccgo_linux_amd64.go
@@ -1,4 +1,4 @@
-// Code generated for linux/amd64 by 'generator -o speedtest1/ccgo_linux_amd64.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/amd64 by 'generator -ignore-unsupported-alignment -o speedtest1/ccgo_linux_amd64.go -I /tmp/libsqlite3/sqlite-src-3450100 /tmp/libsqlite3/sqlite-src-3450100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && amd64
 // +build linux,amd64
