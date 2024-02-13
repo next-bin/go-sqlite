396d6ea3ef396766dc6897db7f29895cbfe22b8a
Jan Mercl <0xjnml@gmail.com>
2024-02-13 12:22:59 +0100

    document v1.0.0 release

--- libsqlite3.go
+++ libsqlite3.go
@@ -4,5 +4,35 @@
 
 //go:generate go run generator.go
 
-// Package libsqlite3 is a ccgo/v4 version the libsqlite3.a (SQLite, http://sqlite.org)
+// Package libsqlite3 is a ccgo/v4 version of libsqlite3 (SQLite, http://sqlite.org)
+//
+// # Supported platforms and architectures
+//
+// These combinations of GOOS and GOARCH are currently supported
+//
+//	OS      Arch    SQLite version
+//	------------------------------
+//	darwin	amd64   3.45.1
+//	darwin	arm64   3.45.1
+//	freebsd	amd64   3.45.1
+//	freebsd	arm64   3.45.1
+//	linux	386     3.45.1
+//	linux	amd64   3.45.1
+//	linux	arm     3.45.1
+//	linux	arm64   3.45.1
+//	linux	ppc64le 3.45.1
+//	linux	riscv64 3.45.1
+//	linux	s390x   3.45.1
+//	windows	amd64   3.45.1
+//	windows	arm64   3.45.1
+//
+// # Builders
+//
+// Builder results available at:
+//
+// https://modern-c.appspot.com/-/builder/?importpath=modernc.org%2flibsqlite3
+//
+// # Changelog
+//
+// 2024-02-13: v1.0.0
 package libsqlite3 // import "modernc.org/libsqlite3"
