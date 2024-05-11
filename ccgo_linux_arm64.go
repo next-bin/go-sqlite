173461bba2b38dcd0111d269aebb96a169faf5c9
Jan Mercl <0xjnml@gmail.com>
2024-05-11 06:27:44 +0200

    pi64: auto generate

--- ccgo_linux_arm64.go
+++ ccgo_linux_arm64.go
@@ -12848,6 +12848,7 @@ func XinflateBack(tls *libc.TLS, strm Tz_streamp, in Tin_func, in_desc uintptr, 
 	_2:
 	}
 	/* Write leftover output and return unused input */
+	goto inf_leave
 inf_leave:
 	;
 	if left < (*Tinflate_state)(unsafe.Pointer(state)).Fwsize {
@@ -12964,6 +12965,7 @@ func Xinflate_fast(tls *libc.TLS, strm Tz_streamp, start uint32) {
 			bits += uint32(8)
 		}
 		here = lcode + uintptr(hold&uint64(lmask))*4
+		goto dolen
 	dolen:
 		;
 		op = uint32((*Tcode)(unsafe.Pointer(here)).Fbits)
@@ -13000,6 +13002,7 @@ func Xinflate_fast(tls *libc.TLS, strm Tz_streamp, start uint32) {
 					bits += uint32(8)
 				}
 				here = dcode + uintptr(hold&uint64(dmask))*4
+				goto dodist
 			dodist:
 				;
 				op = uint32((*Tcode)(unsafe.Pointer(here)).Fbits)
@@ -17006,6 +17009,7 @@ func Xinflate(tls *libc.TLS, strm Tz_streamp, flush int32) (r int32) {
 	   error.  Call updatewindow() to create and/or update the window state.
 	   Note: a memory error from inflate() is non-recoverable.
 	*/
+	goto inf_leave
 inf_leave:
 	;
 	(*Tz_stream)(unsafe.Pointer(strm)).Fnext_out = put
