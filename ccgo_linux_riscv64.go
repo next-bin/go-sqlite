638f60dfcd9a1d78fdfdbcbe960f821536213530
Jan Mercl <0xjnml@gmail.com>
2024-05-11 06:42:33 +0000

    riscv64: auto generate

--- ccgo_linux_riscv64.go
+++ ccgo_linux_riscv64.go
@@ -10770,6 +10770,7 @@ func XinflateBack(tls *libc.TLS, strm Tz_streamp, in Tin_func, in_desc uintptr, 
 	_2:
 	}
 	/* Write leftover output and return unused input */
+	goto inf_leave
 inf_leave:
 	;
 	if left < (*Tinflate_state)(unsafe.Pointer(state)).Fwsize {
@@ -10886,6 +10887,7 @@ func Xinflate_fast(tls *libc.TLS, strm Tz_streamp, start uint32) {
 			bits += uint32(8)
 		}
 		here = lcode + uintptr(hold&uint64(lmask))*4
+		goto dolen
 	dolen:
 		;
 		op = uint32((*Tcode)(unsafe.Pointer(here)).Fbits)
@@ -10922,6 +10924,7 @@ func Xinflate_fast(tls *libc.TLS, strm Tz_streamp, start uint32) {
 					bits += uint32(8)
 				}
 				here = dcode + uintptr(hold&uint64(dmask))*4
+				goto dodist
 			dodist:
 				;
 				op = uint32((*Tcode)(unsafe.Pointer(here)).Fbits)
@@ -14928,6 +14931,7 @@ func Xinflate(tls *libc.TLS, strm Tz_streamp, flush int32) (r int32) {
 	   error.  Call updatewindow() to create and/or update the window state.
 	   Note: a memory error from inflate() is non-recoverable.
 	*/
+	goto inf_leave
 inf_leave:
 	;
 	(*Tz_stream)(unsafe.Pointer(strm)).Fnext_out = put
