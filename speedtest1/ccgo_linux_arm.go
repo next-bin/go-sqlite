5ddf2e469cb0784ff1dd4b938f04953867978aa9
Jan Mercl <0xjnml@gmail.com>
2024-06-16 03:32:29 +0200

    pi32: auto generate

--- speedtest1/ccgo_linux_arm.go
+++ speedtest1/ccgo_linux_arm.go
@@ -1,7 +1,6 @@
 // Code generated for linux/arm by 'generator -DNDEBUG -ignore-unsupported-alignment -o speedtest1/ccgo_linux_arm.go -I /tmp/libsqlite3/sqlite-src-3460000 /tmp/libsqlite3/sqlite-src-3460000/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && arm
-// +build linux,arm
 
 package main
 
