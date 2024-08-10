61675730a913c5e4260eed0e932ecfcacf6733a6
Jan Mercl <0xjnml@gmail.com>
2024-08-10 22:59:52 +0200

    linux/386: generate

--- generator.go
+++ generator.go
@@ -177,7 +177,9 @@ go work use \
 			cflags = append(cflags, "-DTCL_BROKEN_MAINARGS", "-D__CRT__NO_INLINE")
 			util.MustShell(true, nil, "sh", "-c", fmt.Sprintf("CFLAGS='%s' ./configure --build=x86-64_linux --host=i686-w64-mingw32 --enable-64bit --disable-threads --disable-shared --disable-load", strings.Join(cflags, " ")))
 		case
+			target == "linux/386",
 			target == "linux/amd64",
+			target == "linux/arm",
 			target == "linux/arm64",
 			target == "linux/loong64",
 			target == "linux/ppc6464le",
