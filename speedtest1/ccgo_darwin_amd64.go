48f0ef02e2c44fe9af3f8688f225bd2d88f7d76a
Jan Mercl <0xjnml@gmail.com>
2024-08-28 05:41:25 +0200

    darwin: auto generate

--- speedtest1/ccgo_darwin_amd64.go
+++ speedtest1/ccgo_darwin_amd64.go
@@ -1,4 +1,4 @@
-// Code generated for darwin/amd64 by 'generator -DNDEBUG -ignore-unsupported-alignment -o speedtest1/ccgo_darwin_amd64.go -I /var/folders/4f/mc8mts295pqf7gmnfcwh6g8w0000gn/T/libsqlite3/sqlite-src-3460000 /var/folders/4f/mc8mts295pqf7gmnfcwh6g8w0000gn/T/libsqlite3/sqlite-src-3460000/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for darwin/amd64 by 'generator -DNDEBUG -ignore-unsupported-alignment -o speedtest1/ccgo_darwin_amd64.go -I /tmp/libsqlite3/sqlite-src-3460000 /tmp/libsqlite3/sqlite-src-3460000/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build darwin && amd64
 
