9d2f8b4c1c190699d0573a6b90cab5f4e39e1049
Jan Mercl <0xjnml@gmail.com>
2024-11-15 15:33:44 +0000

    cpp.go: fix range check panic

--- v4/cpp.go
+++ v4/cpp.go
@@ -1502,7 +1502,7 @@ func (c *cpp) nextLine() (r textLine) {
 		case controlLine:
 			// trc("%v: controlLine %v", x[0].Position(), toksDump(x))
 			c.pop()
-			if len(x) == 2 {
+			if len(x) < 3 {
 				// eg. ["#" "\n"].2
 				break
 			}
