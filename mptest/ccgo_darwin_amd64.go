3eeb82cea5f521480d6ea9d998eab94c43eae4e7
Jan Mercl <0xjnml@gmail.com>
2025-02-02 22:00:28 +0100

    darwin: auto generate

--- mptest/ccgo_darwin_amd64.go
+++ mptest/ccgo_darwin_amd64.go
@@ -1,4 +1,4 @@
-// Code generated for darwin/amd64 by 'generator -DNDEBUG -I /tmp/libsqlite3/sqlite-src-3460100 -ignore-unsupported-alignment -o mptest/ccgo_darwin_amd64.go /tmp/libsqlite3/sqlite-src-3460100/mptest/mptest.c -lsqlite3', DO NOT EDIT.
+// Code generated for darwin/amd64 by 'generator -DNDEBUG -I /tmp/libsqlite3/sqlite-src-3460100 -ignore-unsupported-alignment -ignore-link-errors -o mptest/ccgo_darwin_amd64.go /tmp/libsqlite3/sqlite-src-3460100/mptest/mptest.c -lsqlite3', DO NOT EDIT.
 
 //go:build darwin && amd64
 
