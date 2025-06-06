018b3d48bccf301b8839fe3f9095f144d7c2c811
Jan Mercl <0xjnml@gmail.com>
2025-06-07 01:46:07 +0800

    linux_loong64b: auto generate

--- speedtest1/ccgo_linux_loong64.go
+++ speedtest1/ccgo_linux_loong64.go
@@ -1,4 +1,4 @@
-// Code generated for linux/loong64 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_loong64.go -I /tmp/libsqlite3/sqlite-src-3500000 /tmp/libsqlite3/sqlite-src-3500000/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/loong64 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_loong64.go -I /tmp/libsqlite3/sqlite-src-3500100 /tmp/libsqlite3/sqlite-src-3500100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && loong64
 
