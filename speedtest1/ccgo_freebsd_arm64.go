bbc5f3cf766d57fba272b4dfe10e4cbfeee6318b
Jan Mercl <0xjnml@gmail.com>
2025-06-07 11:54:57 +0200

    freebsd_arm64: auto generate

--- speedtest1/ccgo_freebsd_arm64.go
+++ speedtest1/ccgo_freebsd_arm64.go
@@ -1,4 +1,4 @@
-// Code generated for freebsd/arm64 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_freebsd_arm64.go -I /tmp/libsqlite3/sqlite-src-3500000 /tmp/libsqlite3/sqlite-src-3500000/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for freebsd/arm64 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_freebsd_arm64.go -I /tmp/libsqlite3/sqlite-src-3500100 /tmp/libsqlite3/sqlite-src-3500100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build freebsd && arm64
 
