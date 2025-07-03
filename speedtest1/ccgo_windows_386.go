723cd33f4d27229bb9e0d01bbfcf8e4f46a6ee99
Jan Mercl <0xjnml@gmail.com>
2025-07-03 14:44:41 +0200

    nuc64: auto generate

--- speedtest1/ccgo_windows_386.go
+++ speedtest1/ccgo_windows_386.go
@@ -1,4 +1,4 @@
-// Code generated for windows/386 by 'generator --cpp /usr/bin/i686-w64-mingw32-gcc --goarch 386 --goos windows -DNDEBUG -DSQLITE_OMIT_SEH -DSQLITE_OS_WIN=1 -I /tmp/libsqlite3/sqlite-src-3500100 -map gcc=i686-w64-mingw32-gcc -o speedtest1/ccgo_windows_386.go /tmp/libsqlite3/sqlite-src-3500100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for windows/386 by 'generator --cpp /usr/bin/i686-w64-mingw32-gcc --goarch 386 --goos windows -DNDEBUG -DSQLITE_OMIT_SEH -DSQLITE_OS_WIN=1 -I /tmp/libsqlite3/sqlite-src-3500200 -map gcc=i686-w64-mingw32-gcc -o speedtest1/ccgo_windows_386.go /tmp/libsqlite3/sqlite-src-3500200/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build windows && 386
 
