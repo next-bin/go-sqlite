b4b6d649d1d42f7f891c6d5528f78d519fe81f88
Jan Mercl <0xjnml@gmail.com>
2025-07-05 10:24:24 +0200

    e5-1650: auto generate

--- speedtest1/ccgo_linux_386.go
+++ speedtest1/ccgo_linux_386.go
@@ -1,4 +1,4 @@
-// Code generated for linux/386 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_386.go -I /tmp/libsqlite3/sqlite-src-3500100 /tmp/libsqlite3/sqlite-src-3500100/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/386 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_386.go -I /tmp/libsqlite3/sqlite-src-3500200 /tmp/libsqlite3/sqlite-src-3500200/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && 386
 
