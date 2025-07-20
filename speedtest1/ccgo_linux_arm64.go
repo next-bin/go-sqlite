b894937c3fcef787ebbe4eba9e90d296cd0c882f
Jan Mercl <0xjnml@gmail.com>
2025-07-20 13:58:05 +0200

    pi64: auto generate

--- speedtest1/ccgo_linux_arm64.go
+++ speedtest1/ccgo_linux_arm64.go
@@ -1,4 +1,4 @@
-// Code generated for linux/arm64 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_arm64.go -I /tmp/libsqlite3/sqlite-src-3500200 /tmp/libsqlite3/sqlite-src-3500200/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/arm64 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_arm64.go -I /tmp/libsqlite3/sqlite-src-3500300 /tmp/libsqlite3/sqlite-src-3500300/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && arm64
 
