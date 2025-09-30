f8498e86fda82e4d092f9f2ff55a9ccf0e7c036c
Jan Mercl <0xjnml@gmail.com>
2025-09-30 05:39:57 +0200

    modernc.org/libsqlite3: e5-1650 auto generate

--- speedtest1/ccgo_linux_386.go
+++ speedtest1/ccgo_linux_386.go
@@ -1,4 +1,4 @@
-// Code generated for linux/386 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_386.go -I /tmp/libsqlite3/sqlite-src-3500300 /tmp/libsqlite3/sqlite-src-3500300/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/386 by 'generator -DNDEBUG -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_386.go -I /tmp/libsqlite3/sqlite-src-3500400 /tmp/libsqlite3/sqlite-src-3500400/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && 386
 
