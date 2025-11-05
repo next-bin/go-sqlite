e5f0fd0228ee2b1f5e51529a242b3f36245de81d
Jan Mercl <0xjnml@gmail.com>
2025-11-05 21:57:45 +0100

    generator.go: update deps

--- generator.go
+++ generator.go
@@ -148,7 +148,7 @@ func main() {
 	ilibtcl := filepath.Join(cwd, "..", "libtcl8.6", "include", goos, goarch)
 	result := "sqlite3.go"
 	util.MustInDir(true, makeRoot, func() (err error) {
-		util.MustShell(true, nil, "sh", "-c", "go mod init example.com/libsqlite3 ; go get modernc.org/libc@v1.66.10 modernc.org/libz@v0.16.22 modernc.org/libtcl8.6@v0.16.10")
+		util.MustShell(true, nil, "sh", "-c", "go mod init example.com/libsqlite3 ; go get modernc.org/libc@v1.66.10 modernc.org/libz@v0.16.23 modernc.org/libtcl8.6@v0.16.11")
 		config := []string{os.Args[0]}
 		if dev {
 			util.MustShell(true, nil, "sh", "-c", "go work init ; go work use $GOPATH/src/modernc.org/libc $GOPATH/src/modernc.org/libz $GOPATH/src/modernc.org/libtcl8.6")
@@ -304,8 +304,8 @@ func main() {
 go mod init example.com/libsqlite3
 go get \
 	modernc.org/libc@v1.66.10 \
-	modernc.org/libtcl8.6@v0.16.10 \
-	modernc.org/libz@v0.16.22 \
+	modernc.org/libtcl8.6@v0.16.11 \
+	modernc.org/libz@v0.16.23 \
 `)
 		if dev {
 			util.MustShell(true, nil, "sh", "-c", `
