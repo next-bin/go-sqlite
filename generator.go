81fc9859e7789a8f01ad00ccb98c8173b31510ab
Jan Mercl <0xjnml@gmail.com>
2026-03-13 15:40:33 +0100

    upgrade to SQLite 3.51.3, updates sqlite#247

--- generator.go
+++ generator.go
@@ -25,7 +25,7 @@ import (
 const (
 	archivePath  = "sqlite-amalgamation-" + versionTag + ".zip"
 	archive2Path = "sqlite-src-" + versionTag + ".zip"
-	versionTag   = "3520000"
+	versionTag   = "3510300"
 )
 
 var (
