8c3946690bd376563fe9f27306b797f411232ee8
Jan Mercl <0xjnml@gmail.com>
2026-04-06 18:13:09 +0200

    modernc.org/libsqlite3: openbsd_arm64 auto generate

--- speedtest1/ccgo_openbsd_arm64.go
+++ speedtest1/ccgo_openbsd_arm64.go
@@ -1,4 +1,4 @@
-// Code generated for openbsd/arm64 by 'generator -DNDEBUG -DSQLITE_ENABLE_RTREE -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_openbsd_arm64.go -I /tmp/libsqlite3/sqlite-src-3510300 /tmp/libsqlite3/sqlite-src-3510300/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for openbsd/arm64 by 'generator -DNDEBUG -DSQLITE_ENABLE_DBPAGE_VTAB -DSQLITE_ENABLE_RTREE -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_openbsd_arm64.go -I /tmp/libsqlite3/sqlite-src-3510300 /tmp/libsqlite3/sqlite-src-3510300/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build openbsd && arm64
 
