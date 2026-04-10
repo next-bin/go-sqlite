ad99b9c059fb27b5700dafc78f67858a39e62771
Jan Mercl <0xjnml@gmail.com>
2026-04-10 22:34:35 +0200

    modernc.org/libsqlite3: nuc64 auto generate

--- speedtest1/ccgo_linux_amd64.go
+++ speedtest1/ccgo_linux_amd64.go
@@ -1,4 +1,4 @@
-// Code generated for linux/amd64 by 'generator -DNDEBUG -DSQLITE_ENABLE_DBPAGE_VTAB -DSQLITE_ENABLE_RTREE -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_amd64.go -I /tmp/libsqlite3/sqlite-src-3510300 /tmp/libsqlite3/sqlite-src-3510300/test/speedtest1.c -lsqlite3', DO NOT EDIT.
+// Code generated for linux/amd64 by 'generator -DNDEBUG -DSQLITE_ENABLE_DBPAGE_VTAB -DSQLITE_ENABLE_RTREE -ignore-unsupported-alignment -ignore-link-errors -o speedtest1/ccgo_linux_amd64.go -I /tmp/libsqlite3/sqlite-src-3530000 /tmp/libsqlite3/sqlite-src-3530000/test/speedtest1.c -lsqlite3', DO NOT EDIT.
 
 //go:build linux && amd64
 
